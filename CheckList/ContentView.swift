54797f0d77814ab6cd46184fc437394f531c4432
zaid <alahat@uwp.edu>
2020-02-23 08:14:29 -0600

    simple list wtih sections

--- CheckList/ContentView.swift
+++ CheckList/ContentView.swift
@@ -11,13 +11,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            /*
+             There are two styles of List: plain and grouped.
+             Right now, the app’s List uses the plain style.
+             Use the plain style for lists where all the items in the list are
+             similar to one another, yet independent.
+             Using the grouped style requires the following:
+             1. Using List’s listStyle() to specify that the list should use
+             the grouped style.
+             2. Adding a Section inside the List for each group,
+             which appears as a separate sublist within the list.
+             Sections can contain their own headers.
+             */
+            //## Test
             List {
-                Text("Walk the dog")
-                Text("Brush my teeth")
-                Text("Learn iOS development")
-                Text("Soccer practice")
-                Text("Eat ice cream")
+                Section(header: Text("High priority")) {
+                    Text("Walk the dog")
+                    Text("Brush my teeth")
+                    Text("Learn iOS development")
+                }
+                Section(header: Text("Low priority")) {
+                    Text("Soccer practice")
+                    Text("Eat ice cream")
+                }
             }
+            .listStyle(GroupedListStyle())
             .navigationBarTitle("Checklist")
         }
     }
