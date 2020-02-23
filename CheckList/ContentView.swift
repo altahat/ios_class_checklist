9ff621847c412b8ecfd88ff6536398c7e88e5d23
zaid <alahat@uwp.edu>
2020-02-23 10:57:11 -0600

    Fix the immutability issue with @State

--- CheckList/ContentView.swift
+++ CheckList/ContentView.swift
@@ -12,8 +12,14 @@ struct ContentView: View {
     /*
      Use Arrays.
      Let's respond to actions when an item is clicked.
+     We have a problem: The ’self is immutable’ error message.
+     Reason:
+     *By default*, code inside a struct object is not allowed to change the
+     values of that struct’s properties.
+     *Normally*, only code outside the struct is allowed to do that.
+     One way  to change that is the use of @State
      */
-    var checklistItems = ["Walk the dog", "Brush my teeth", "Learn",
+    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn",
                           "Soccer practice", "Eat ice cream"]
     var body: some View {
         NavigationView {
