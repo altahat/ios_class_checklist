dfdccb3941c00bb219b865bbd767759b4d27d92e
zaid <alahat@uwp.edu>
2020-02-23 10:49:16 -0600

    Add array

--- CheckList/ContentView.swift
+++ CheckList/ContentView.swift
@@ -9,55 +9,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    /*
+     Use Arrays.
+     */
+    var checklistItems = ["Walk the dog", "Brush my teeth", "Learn",
+                          "Soccer practice", "Eat ice cream"]
     var body: some View {
         NavigationView {
-            /*
-             In a static list, the simplest way to get around this limitation
-             is to use a Group Its sole purpose is to provide a way for you to
-             treat a group of two to ten views as a single view.
-             Now Error gone with 11 views
-             Fix it by using Group
-             */
             List {
-                Section(
-                header: Text("High priority")) {
-                    Group {
-                        Text("Walk the dog")
-                        Text("Brush my teeth")
-                        Text("Learn iOS development")
-                        Text("Make dinner")
-                        Text("Do laundry")
-                        Text("Pay bills")
-                    }
-                    Group {
-                        Text("Finish homework")
-                        Text("Change internet provider")
-                        Text("Read RayWenderlich.com")
-                        Text("Clean the kitchen")
-                        Text("Wash the car")
-                    } }
-                Section(
-                header: Text("Low priority")) {
-                    Text("Soccer practice")
-                    Text("Eat ice cream")
-                    Text("Take vocal lessons")
-                    Text("Record hit single")
-                    Text("Learn every martial art")
-                    Text("Design costume")
-                    Text("Design crime-fighting vehicle")
-                    Text("Come up with superhero name")
-                    Text("Befriend space raccoon")
-                    Text("Save the world")
-                } }
-                .listStyle(GroupedListStyle())
-                .navigationBarTitle("Checklist")
-            
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+                Text(checklistItems[0])
+                Text(checklistItems[1])
+                Text(checklistItems[2])
+                Text(checklistItems[3])
+                Text(checklistItems[4])
+            } .navigationBarTitle("Checklist")
+        } }
 }
