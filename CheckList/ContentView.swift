7cf1c2cf5889c893bd12657e2726333f0b72afb5
zaid <alahat@uwp.edu>
2020-02-23 11:02:55 -0600

    print the list on onAppear

--- CheckList/ContentView.swift
+++ CheckList/ContentView.swift
@@ -10,27 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     /*
-     Use Arrays.
-     Let's respond to actions when an item is clicked.
-     We have a problem: The ’self is immutable’ error message.
-     Reason:
-     *By default*, code inside a struct object is not allowed to change the
-     values of that struct’s properties.
-     *Normally*, only code outside the struct is allowed to do that.
-     One way  to change that is the use of @State
+     Use Loops.
+     Let's print the list and do something on onAppear
      */
-    @State var checklistItems = ["Walk the dog", "Brush my teeth", "Learn",
-                          "Soccer practice", "Eat ice cream"]
+    @State var checklistItems = [
+        "Walk the dog",
+        "Brush my teeth",
+        "Learn iOS development",
+        "Soccer practice",
+        "Eat ice cream"
+    ]
     var body: some View {
         NavigationView {
             List {
-                Text(checklistItems[0])
-                    .onTapGesture {
-                        self.checklistItems[0] = "Take the dog to the vet" }
-                Text(checklistItems[1])
-                Text(checklistItems[2])
-                Text(checklistItems[3])
-                Text(checklistItems[4])
-            } .navigationBarTitle("Checklist")
+                Text("Nothing to see here...yet!") }
+                .navigationBarTitle("Checklist")
+                .onAppear() {
+                    self.printChecklistContents() }
         } }
+    func printChecklistContents() {
+        for item in checklistItems {
+            print(item)
+        }
+    }
 }
