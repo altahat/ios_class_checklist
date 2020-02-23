9c9c0c23598baa2b8732f5da8200b70dcbf77b8f
zaid <alahat@uwp.edu>
2020-02-23 07:45:48 -0600

    Initial Commit

---
+++ CheckList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckList
+//
+//  Created by zaid on 2/23/20.
+//  Copyright © 2020 UW Parkside. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
