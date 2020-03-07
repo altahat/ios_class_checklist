2a8b157797633c5d5a5ba68f00bb4efdf6b1deb9
zaid <alahat@uwp.edu>
2020-03-07 14:54:07 -0600

    Automatically dismiss the sheet after adding  an item.

--- CheckList/NewChecklistItemView.swift
+++ CheckList/NewChecklistItemView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 
 struct NewChecklistItemView: View {
     @State var newItemName = ""
+    /*
+     @Environment marks a property as one that can access a specific system setting related
+     to the operating system environment, hence the name. It lets you find useful settings,
+     such as whether the user’s language is left-to-right or right-to-left,
+     which calendar system is appropriate for the user’s locale settings,
+     the current color scheme and other system-wide settings.
+     */
+    @Environment(\.presentationMode) var presentationMode
     var checklist: Checklist
     
     var body: some View {
@@ -21,6 +29,7 @@ struct NewChecklistItemView: View {
                     let newChecklistItem = ChecklistItem(name: self.newItemName)
                     self.checklist.items.append(newChecklistItem)
                     self.checklist.printChecklistContents()
+                    self.presentationMode.wrappedValue.dismiss()
                 }) { HStack {
                     Image(systemName: "plus.circle.fill")
                     Text("Add new item")
